5ece08f3a56592d9d3e0c9ae87c969eaf4a5a86d
syamjith45 <syamjith0505@gmail.com>
2023-12-11 15:57:04 +0530

    third commit

--- cart/cart.py
+++ cart/cart.py
@@ -12,3 +12,15 @@ class Cart():
 
         # Make sure that 'cart' is available on all pages
         self.cart = cart
+    
+    def add(self,product):
+        product_id = str(product.id)
+        
+        
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+            
+        self.session.modified = True
+        
--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 # Create your views here.
 def cart_summary(request):
     return render(request,"cart_summary.html",{})
 def cart_add(request):
+    #get the cart
+    cart= Cart(request)
+    if request.POST.get('action')=='post':
+        
+        product_id = int(request.POST.get('product_id'))
+        # lookup product in DB
+        product= get_object_or_404(Product,id=product_id)
+        
+        cart.add(product=product)
+        
+        response=JsonResponse({'Product Name: ': product.p_name})
+        return response
+        
+    
     return render(request,"cart_summary.html")
 def cart_delete(request):
     return render(request,"cart_summary.html")
