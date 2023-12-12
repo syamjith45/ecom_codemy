9dcfd63e54734f737cb375775053e23371829877
syamjith45 <syamjith0505@gmail.com>
2023-12-12 15:04:45 +0530

    fourth commit

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,6 @@
+from store.models import Product
+
+
 class Cart():
     def __init__(self, request) -> None:
         # Get the current session from the request
@@ -16,11 +19,20 @@ class Cart():
     def add(self,product):
         product_id = str(product.id)
         
-        
         if product_id in self.cart:
             pass
         else:
             self.cart[product_id] = {'price': str(product.price)}
             
         self.session.modified = True
+    
+    def __len__(self):
         
+        return len(self.cart)
+    
+    
+    def get_prods(self):
+        product_ids=self.cart.keys()
+        products=Product.objects.filter(id__in=product_ids)
+        
+        return products
--- cart/views.py
+++ cart/views.py
@@ -4,19 +4,23 @@ from store.models import Product
 from django.http import JsonResponse
 # Create your views here.
 def cart_summary(request):
-    return render(request,"cart_summary.html",{})
+    cart=Cart(request)
+    cart_products = cart.get_prods
+    return render(request,"cart_summary.html",{'cart_products':cart_products})
 def cart_add(request):
     #get the cart
     cart= Cart(request)
     if request.POST.get('action')=='post':
-        
+         
         product_id = int(request.POST.get('product_id'))
         # lookup product in DB
         product= get_object_or_404(Product,id=product_id)
         
         cart.add(product=product)
-        
-        response=JsonResponse({'Product Name: ': product.p_name})
+        #get quantity
+        cart_quantity=cart.__len__()
+        # response=JsonResponse({'Product Name: ': product.p_name})
+        response=JsonResponse({'qty': cart_quantity})
         return response
         
     
